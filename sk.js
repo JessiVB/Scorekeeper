27e335aa847e62a1f19191e7b25b59536178593f
Jess <44506244+JessiVB@users.noreply.github.com>
2019-04-23 13:42:22 -0400

    Add files via upload

--- sk.js
+++ sk.js
@@ -10,10 +10,7 @@ document.querySelector('#sub1').onclick = minusOne;
 // parseInt(#zer0)
 // parseInt(#add5)
 
-// console.log(#zero);
-// console.log(#add5);
-// Hint: use document.querySelector and .onclick for each button
-// 2. Write functions for each click handler (ex: addFive, addTen, minusOne)
+
 let total = 0;
 
 function zero() {
@@ -36,10 +33,3 @@ function minusOne() {
     total = total - 1;
     document.querySelector('#result').innerHTML = total;
 }
-// 3. Each function will need to: 
-// A) Use the .innerHTML property to get the value inside of #result (store as a variable)
-// B) Manipulate the value using the parseInt() method
-// C) Perform the correct arithmetic
-// D) Use the .innerHTML property to write the new value to #result
-
-// E) WE'll DO LATER - Create an input element and add that value to the total#a
